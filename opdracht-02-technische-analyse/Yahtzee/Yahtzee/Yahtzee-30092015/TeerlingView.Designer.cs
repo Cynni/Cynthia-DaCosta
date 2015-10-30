3666f4066a339e325e11e4bc7ead3bb4849a97d5
Cynthia <cynthia.dacosta@student.kdg.be>
2015-10-30 23:18:10 +0100

    Bestanden Toegevoegd

---
+++ opdracht-02-technische-analyse/Yahtzee/Yahtzee/Yahtzee-30092015/TeerlingView.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Yahtzee_30092015
+{
+  partial class TeerlingView
+  {
+    /// <summary> 
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary> 
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Component Designer generated code
+
+    /// <summary> 
+    /// Required method for Designer support - do not modify 
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.btnWerpTeeriling = new System.Windows.Forms.Button();
+      this.lblTeerling = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // btnWerpTeeriling
+      // 
+      this.btnWerpTeeriling.Location = new System.Drawing.Point(37, 118);
+      this.btnWerpTeeriling.Name = "btnWerpTeeriling";
+      this.btnWerpTeeriling.Size = new System.Drawing.Size(75, 23);
+      this.btnWerpTeeriling.TabIndex = 0;
+      this.btnWerpTeeriling.Text = "Werp";
+      this.btnWerpTeeriling.UseVisualStyleBackColor = true;
+      this.btnWerpTeeriling.Click += new System.EventHandler(this.btnWerpTeeriling_Click);
+      // 
+      // lblTeerling
+      // 
+      this.lblTeerling.Location = new System.Drawing.Point(8, 5);
+      this.lblTeerling.Name = "lblTeerling";
+      this.lblTeerling.Size = new System.Drawing.Size(133, 110);
+      this.lblTeerling.TabIndex = 1;
+      this.lblTeerling.Text = "label1";
+      this.lblTeerling.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+      this.lblTeerling.Click += new System.EventHandler(this.lblTeerling_Click);
+      // 
+      // TeerlingView
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.Controls.Add(this.lblTeerling);
+      this.Controls.Add(this.btnWerpTeeriling);
+      this.Name = "TeerlingView";
+      this.Load += new System.EventHandler(this.TeerlingView_Load);
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button btnWerpTeeriling;
+    private System.Windows.Forms.Label lblTeerling;
+  }
+}
---
+++ opdracht-02-technische-analyse/Yahtzee/Yahtzee/Yahtzee-30092015/TeerlingView.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Yahtzee_30092015
+{
+  public partial class TeerlingView : UserControl
+  {
+    private TeerlingController controller;
+    private int aantalKerenGeworpen = 0;
+
+    public TeerlingView(TeerlingController c)
+    {
+      InitializeComponent();
+      controller = c;
+    }
+
+    public void UpdateUI()
+    {
+      lblTeerling.Text = controller.model.AantalOgen.ToString();
+    }
+
+    private void btnWerpTeeriling_Click(object sender, EventArgs e)
+    {
+      controller.Werp();
+      aantalKerenGeworpen++;
+      int nieuwAantalOgen = controller.model.AantalOgen;
+      lblTeerling.Text = nieuwAantalOgen.ToString();
+      if (aantalKerenGeworpen == 1)
+      {
+        btnWerpTeeriling.Visible = false;
+      }   
+    }
+
+    private void TeerlingView_Load(object sender, EventArgs e)
+    {
+      btnWerpTeeriling.Visible = false;
+    }
+
+    public void cheatButtonVisibility()
+    {
+      btnWerpTeeriling.Visible = true;
+    }
+
+    public void ColorBackGroundLabel()
+    {
+      lblTeerling.BackColor = Color.Red; 
+    }
+
+    public bool CheckColorLabel()
+    {
+      if (lblTeerling.BackColor == Color.Red)
+      {
+        return true;
+      }
+      else { return false; }
+    }
+
+    private void lblTeerling_Click(object sender, EventArgs e)
+    {
+      ColorBackGroundLabel();
+    }
+  }
+
+}
